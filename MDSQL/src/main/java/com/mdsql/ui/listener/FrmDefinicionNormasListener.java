69b177b52f8a8bf3a0ad87ebbe4f9f97194c3375
HernanCarrenoInetum <hernan.carreno.ext@inetum.com>
2022-09-07 18:10:52 +0200

    call example TODO remove mock input and oputput data

--- MDSQL/src/main/java/com/mdsql/ui/listener/FrmDefinicionNormasListener.java
+++ MDSQL/src/main/java/com/mdsql/ui/listener/FrmDefinicionNormasListener.java
@@ -2,6 +2,9 @@ package com.mdsql.ui.listener;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -10,8 +13,11 @@ import java.util.Observer;
 
 import javax.swing.JButton;
 
+import com.mdsql.bussiness.entities.InputSeleccionarProcesados;
 import com.mdsql.bussiness.entities.Norma;
+import com.mdsql.bussiness.entities.Proceso;
 import com.mdsql.bussiness.service.NormaService;
+import com.mdsql.bussiness.service.ProcesoService;
 import com.mdsql.ui.model.DefinicionNormasTableModel;
 import com.mdsql.ui.normasnomenclatura.FrmDefinicionNormas;
 import com.mdsql.ui.utils.ListenerSupport;
@@ -76,6 +82,56 @@ public class FrmDefinicionNormasListener extends ListenerSupport implements Acti
 	 * @return lista de glosarios que cumple con el termino buscado
 	 */
 	private List<Norma> buscar(String termino) {
+
+		//TODO eliminar ejemplo llamado a nuevo servicio
+		ProcesoService procesoService = (ProcesoService) getService("procesoService");
+		//mockear datos de entrada si es necesario o rellenarlos directamente desde los inputs de las pantallas
+		InputSeleccionarProcesados mockInputExample = InputSeleccionarProcesados
+				.builder()
+				.pCodigoPeticion("codigoPeticion")
+				.pCodigoUsuarioPeticion("codigoSubProyecto")
+				.pCodigoUsuarioPeticion("usuario")
+				.pFechaInicio(new Date())
+				.pFechaFin(new Date())
+				.pCodigoUsuario("codigousuario")
+				.pCodigoproyecto("pCodigoproyecto")
+				.pCodigoSubProyecto("pCodigoSubProyecto")
+				.pDescripcionEstadoProceso("pDescripcionEstadoProceso")
+				.pUltimas(new BigDecimal("0.00"))
+				.build();
+		//List<Proceso> procesos = procesoService.seleccionarProcesados(mockInputExample); //llamada real
+
+		//mock datos de salida ejemplo:
+		List<Proceso> procesosListMock = new ArrayList<>();
+		Proceso procesoMock1 = Proceso.builder()
+				.idProceso(new BigDecimal("1"))
+				.codigoPeticion("codigo1")
+				.codigoUsrPeticion("user1")
+				.fechaInicio(new Date())
+				.codigoUsr("codigousr1")
+				.codigoEstadoProceso(new BigDecimal("1"))
+				.descripcionEstadoProceso("descripcion 1")
+				.mcaInicial("mca inicial 1")
+				.txtDescripcion("descripcion 1")
+				.txtObservacionEntrega("observacion 1")
+				.mcaErrores("mca error 1 ")
+				.build();
+		Proceso procesoMock2 = Proceso.builder()
+				.idProceso(new BigDecimal("2"))
+				.codigoPeticion("codigo2")
+				.codigoUsrPeticion("user2")
+				.fechaInicio(new Date())
+				.codigoUsr("codigousr2")
+				.codigoEstadoProceso(new BigDecimal("2"))
+				.descripcionEstadoProceso("descripcion 2")
+				.mcaInicial("mca inicial 2")
+				.txtDescripcion("descripcion 2")
+				.txtObservacionEntrega("observacion 2")
+				.mcaErrores("mca error 2")
+				.build();
+		procesosListMock.add(procesoMock1);
+		procesosListMock.add(procesoMock2);
+
 		NormaService normaService = (NormaService) getService(Constants.NORMA_SERVICE);
 		List<Norma> normas = normaService.consultaNormas(termino);
 		return normas;
