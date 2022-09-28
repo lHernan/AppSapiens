1fcb22f6eb4e48ad2fcb97a6683bdb2d5b0bd7b3
Juan Carlos <juancarlosjalonso2002@gmail.com>
2022-09-28 10:48:20 +0200

    PantallaEjecutarTypes

--- MDSQL/src/main/java/com/mdsql/ui/listener/PantallaEjecutarTypesActionListener.java
+++ MDSQL/src/main/java/com/mdsql/ui/listener/PantallaEjecutarTypesActionListener.java
@@ -2,15 +2,23 @@ package com.mdsql.ui.listener;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.List;
+import java.util.Map;
 
 import javax.swing.JButton;
 
 import org.apache.commons.lang3.StringUtils;
 
+import com.mdsql.bussiness.entities.SeleccionHistorico;
+import com.mdsql.bussiness.service.ProcesoService;
+import com.mdsql.bussiness.service.TypeService;
+import com.mdsql.exceptions.ServiceException;
 import com.mdsql.ui.PantallaEjecutarTypes;
 import com.mdsql.ui.model.EjecutarTypesTableModel;
 import com.mdsql.ui.model.ProcesarScriptNotaTableModel;
+import com.mdsql.ui.model.SeleccionHistoricoTableModel;
 import com.mdsql.ui.utils.ListenerSupport;
+import com.mdsql.ui.utils.MDSQLUIHelper;
 import com.mdsql.utils.Constants;
 
 public class PantallaEjecutarTypesActionListener extends ListenerSupport implements ActionListener {
@@ -78,6 +86,26 @@ public class PantallaEjecutarTypesActionListener extends ListenerSupport impleme
 	
 	public void eventBtnCancelar() {
 		pantallaEjecutarTypes.dispose();
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	public void onLoad() {
+		try {
+			TypeService typeService = (TypeService) getService(Constants.PROCESO_SERVICE);
+
+			String typeProyecto = (String) pantallaEjecutarTypes.getParams().get("codigoProyecto");
+
+			// TODO - Preguntar cómo añadir los objetos nuevos
+
+			if (checkAllConfigured(
+					(EjecutarTypesTableModel) pantallaEjecutarTypes.getTblTypes().getModel())) {
+				pantallaEjecutarTypes.getBtnRechazar().setEnabled(Boolean.FALSE);
+			}
+		} catch (ServiceException e) {
+			Map<String, Object> params = MDSQLUIHelper.buildError(e);
+			MDSQLUIHelper.showPopup(pantallaEjecutarTypes.getFrameParent(), Constants.CMD_ERROR, params);
+		}
 	}
 	
 }
