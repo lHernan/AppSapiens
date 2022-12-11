3676616ca7298a5abaa89693fdaf8eaa4b3ed7fc
Federico Martín Lara <federico@horizontefinal>
2022-12-11 23:46:43 +0100

    Componente check para la cabecera de las tablas

---
+++ mdcommon/src/main/java/com/mdval/ui/model/cabeceras/TableItemListener.java
@@ -0,0 +1,32 @@
+package com.mdval.ui.model.cabeceras;
+
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+
+import javax.swing.AbstractButton;
+
+import com.mdval.ui.utils.TableSupport;
+
+public class TableItemListener implements ItemListener {
+
+	private TableSupport table;
+
+	public TableItemListener(TableSupport table) {
+		super();
+		this.table = table;
+	}
+
+	@Override
+	public void itemStateChanged(ItemEvent e) {
+		Object source = e.getSource();
+		
+		if (source instanceof AbstractButton == false)
+			return;
+		
+		boolean checked = e.getStateChange() == ItemEvent.SELECTED;
+		for (int x = 0, y = table.getRowCount(); x < y; x++) {
+			table.setValueAt(new Boolean(checked), x, 0);
+		}
+	}
+
+}
