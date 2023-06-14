b3693e94c57a46037ef454936c50c235a58c4745
Federico Martín Lara <federico@horizontefinal>
2023-06-15 00:15:45 +0200

    Se resuelve:
    - 0000064: Pantalla Ejecutar Scripts CON Historico-Generar Historico 1

--- MDSQL/src/main/java/com/mdsql/ui/listener/PantallaProcesarScriptActionListener.java
+++ MDSQL/src/main/java/com/mdsql/ui/listener/PantallaProcesarScriptActionListener.java
@@ -256,10 +256,11 @@ public class PantallaProcesarScriptActionListener extends ListenerSupport implem
 
 			// Si el modelo solo tiene un solo submodelo, se seleccionará directamente en el
 			// combo.
-			if (subProyectos.size() == 1) {
+			// TODO - Preguntar a Mario si debe haber siempre uno seleccionado
+			//if (subProyectos.size() == 1) {
 				pantallaProcesarScript.setSubproyectoSeleccionado(subProyectos.get(0));
 				pantallaProcesarScript.getCmbSubmodelo().setSelectedItem(subProyectos.get(0));
-			}
+			//}
 		}
 	}
 
