dbce2f2cebf4b96156df121febba1e99df47920e
Federico Martín Lara <federico@horizontefinal>
2023-08-23 10:34:53 +0200

    scripts SQL

--- SQL/ejercicio-9.sql
+++ SQL/ejercicio-9.sql
@@ -1,19 +1,15 @@
 --PRACTICA4- ASTERISCOS
 DECLARE
-nombre     VARCHAR2(100);
-asteriscos VARCHAR2(100);
+    nombre     VARCHAR2(100);
+    asteriscos VARCHAR2(100);
 BEGIN
-nombre := 'ALBERTO';
-FOR i IN 1..length(nombre) LOOP
-asteriscos := asteriscos || '*';
-END LOOP;
+    nombre := 'ALBERTO';
+    FOR i IN 1..length(nombre) LOOP
+        asteriscos := asteriscos || '*';
+    END LOOP;
 
-dbms_output.put_line(nombre
-|| '-->'
-|| asteriscos);
-END;
-/
-                     || '-->'
+    dbms_output.put_line(nombre
+                         || '-->'
                          || asteriscos);
 END;
 /
