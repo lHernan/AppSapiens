36d36265d8a7e24758b77f8ca4f071e25bcdd503
Federico Martín Lara <federico@horizontefinal>
2023-09-14 10:06:33 +0200

    Update query

--- bin/resueltas.sh
+++ bin/resueltas.sh
@@ -8,4 +8,4 @@ CMD="/usr/bin/mysql -D bugtracker "
 
 OPTS=""
 	
-$CMD $OPTS -e "SELECT c.name as 'categoria', LPAD(b.id, 7, \"0\") as 'ID', b.version as 'viene de' ,b.summary as 'descripcion' FROM bugtracker.mantis_bug_table b inner join bugtracker.mantis_category_table c on b.category_id = c.id where fixed_in_version = '$1'"
+$CMD $OPTS -e "SELECT c.name as 'categoria', LPAD(b.id, 7, \"0\") as 'ID', b.version as 'viene de' ,b.summary as 'descripcion' FROM bugtracker.mantis_bug_table b inner join bugtracker.mantis_category_table c on b.category_id = c.id where b.fixed_in_version = '$1' and b.status = 80"
