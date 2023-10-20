fc4e038b141b3612208a8d3df66c4d24e9045703
MAMALAFE <f.martin@ibermatica.com>
2023-10-20 22:03:48 +0200

    Logica PantallaMantenimientoNotasModelos

--- MDSQL/src/main/java/com/mdsql/ui/listener/tables/NotasModeloTableListener.java
+++ MDSQL/src/main/java/com/mdsql/ui/listener/tables/NotasModeloTableListener.java
@@ -6,6 +6,8 @@ import com.mdsql.ui.PantallaProcesarScript;
 import com.mdsql.ui.model.NotasModeloTableModel;
 import com.mdsql.ui.model.ProcesarScriptNotaTableModel;
 import com.mdsql.ui.utils.ListenerSupport;
+import com.mdsql.utils.DateFormatter;
+import com.mdval.utils.AppHelper;
 import com.mdval.utils.LogWrapper;
 import lombok.extern.slf4j.Slf4j;
 
@@ -19,9 +21,13 @@ public class NotasModeloTableListener extends ListenerSupport implements ListSel
 
 	private PantallaMantenimientoNotasModelos pantallaMantenimientoNotasModelos;
 
+	private DateFormatter dateFormatter;
+
 	public NotasModeloTableListener(PantallaMantenimientoNotasModelos pantallaMantenimientoNotasModelos) {
 		super();
 		this.pantallaMantenimientoNotasModelos = pantallaMantenimientoNotasModelos;
+
+		dateFormatter = new DateFormatter();
 	}
 
 	@Override
@@ -38,6 +44,24 @@ public class NotasModeloTableListener extends ListenerSupport implements ListSel
 		if (!Objects.isNull(seleccionado)) {
 			LogWrapper.debug(log, "Selected: %s", seleccionado.toString());
 			pantallaMantenimientoNotasModelos.setAvisoSeleccionado(seleccionado);
+
+			pantallaMantenimientoNotasModelos.getTxtPeticion().setText(seleccionado.getCodigoPeticion());
+			pantallaMantenimientoNotasModelos.getCmbImportancia().setSelectedItem(seleccionado.getNivelImportancia());
+			pantallaMantenimientoNotasModelos.getTxtTitulo().setText(seleccionado.getTxtAviso());
+			pantallaMantenimientoNotasModelos.getTxtDescripcion().setText(seleccionado.getDescripcionAviso());
+			pantallaMantenimientoNotasModelos.getTxtUsuarioAlta().setText(seleccionado.getCodigoUsrAlta());
+
+			String sFecha = dateFormatter.dateToString(seleccionado.getFechaAlta());
+			pantallaMantenimientoNotasModelos.getTxtFechaAlta().setText(sFecha);
+
+			pantallaMantenimientoNotasModelos.getTxtUsuarioModificacion().setText(seleccionado.getCodigoUsuario());
+			sFecha = dateFormatter.dateToString(seleccionado.getFechaActualizacion());
+			pantallaMantenimientoNotasModelos.getTxtFechaModificacion().setText(sFecha);
+
+			Boolean habilitado = AppHelper.normalizeCheckValue(seleccionado.getMcaHabilitado());
+			pantallaMantenimientoNotasModelos.getChkHabilitada().setSelected(habilitado);
+
+			pantallaMantenimientoNotasModelos.getBtnGuardar().setEnabled(Boolean.TRUE);
 		}
 	}
 
