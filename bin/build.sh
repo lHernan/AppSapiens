c33a140a7b3b7332f1e7c777f672d6dc0050cbe0
MAMALAFE <f.martin@ibermatica.com>
2023-10-27 09:10:25 +0200

    Borrar versiones anteriores

--- bin/build.sh
+++ bin/build.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+if [[ "$1" == "" ]] ; then
+	echo "Proporcione una rama para entregar (develop o Fase_3)"
+	exit
+fi
+
 DIR_PROJECT=/home/jmartin/git/AppSapiens
 RAMA=$1
 
@@ -21,7 +26,6 @@ CC=agarcia@esoluzion.com,fmartin@esoluzion.com,Maria.De-Vera@sapiens.com,jmjimen
 clear
 
 if [[ "$LATEST" == "" ]] ; then
-	#notify-send --urgency=low "No hay versión liberada aún"
 	echo "No hay versión liberada aún"
 	exit
 fi
@@ -35,7 +39,6 @@ cd $DIR_PROJECT/$MDCOMMON
 
 mvn clean install
 if [ $? -ne 0 ] ; then
-	#notify-send --urgency=low "Ocurrió un error al construir la arquitectura común"
 	echo "Ocurrió un error al construir la arquitectura común"
 	exit
 fi
@@ -48,7 +51,6 @@ cd $DIR_PROJECT/$MDSQL_PROJECT
 
 mvn clean install -DskipTests=true
 if [ $? -ne 0 ] ; then
-	#notify-send --urgency=low "Ocurrió un error al construir el proyecto $MDSQL_PROJECT"
 	echo "Ocurrió un error al construir el proyecto $MDSQL_PROJECT"
 	exit
 fi
@@ -56,6 +58,10 @@ fi
 clear
 
 echo "Instalando $MDSQL_PROJECT"
+
+rm *.zip
+
+[ ! -d "$DIR_PROJECT/$ENTREGABLES" ] && mkdir $DIR_PROJECT/$ENTREGABLES
 
 cd $DIR_PROJECT/$MDSQL_PROJECT/$BUILD_DIR
 
@@ -66,35 +72,30 @@ cd $DIR_PROJECT/$MDSQL_PROJECT
 
 cp CHANGELOG.md $DIR_PROJECT/$ENTREGABLES/$MDSQL_PROJECT
 if [ $? -ne 0 ] ; then
-	#notify-send --urgency=low "Ocurrió un error al copiar el fichero CHANGELOG.md"
 	echo "Ocurrió un error al copiar el fichero CHANGELOG.md"
 	exit
 fi
 
 cp literales.properties $DIR_PROJECT/$ENTREGABLES/$MDSQL_PROJECT
 if [ $? -ne 0 ] ; then
-	#notify-send --urgency=low "Ocurrió un error al copiar el fichero literales.properties"
 	echo "Ocurrió un error al copiar el fichero literales.properties"
 	exit
 fi
 
 cp configuration.properties $DIR_PROJECT/$ENTREGABLES/$MDSQL_PROJECT
 if [ $? -ne 0 ] ; then
-	#notify-send --urgency=low "Ocurrió un error al copiar el fichero configuration.properties"
 	echo "Ocurrió un error al copiar el fichero configuration.properties"
 	exit
 fi
 
 cp logotipo.png $DIR_PROJECT/$ENTREGABLES/$MDSQL_PROJECT
 if [ $? -ne 0 ] ; then
-	#notify-send --urgency=low "Ocurrió un error al copiar el fichero logotipo.png"
 	echo "Ocurrió un error al copiar el fichero logotipo.png"
 	exit
 fi
 
 git add .
 if [ $? -ne 0 ] ; then
-	#notify-send --urgency=low "Ocurrió un error al añadir archivos para commit"
 	echo "Ocurrió un error al añadir archivos para commit"
 	exit
 fi
@@ -109,30 +110,27 @@ cd $DIR_PROJECT/$ENTREGABLES/
 
 zip -9 -r $MDSQL_PROJECT.zip $MDSQL_PROJECT/
 if [ $? -ne 0 ] ; then
-	#notify-send --urgency=low "No se ha podido crear el paquete $MDSQL_PROJECT.zip"
 	echo "No se ha podido crear el paquete $MDSQL_PROJECT.zip"
 	exit
 fi
 
 mv $MDSQL_PROJECT.zip $MDSQL_PROJECT-$LATEST.zip
+rm -r $DIR_PROJECT/$ENTREGABLES
 
 git add $MDSQL_PROJECT-$LATEST.zip
 if [ $? -ne 0 ] ; then
-	#notify-send --urgency=low "Ocurrió un error al añadir archivo $MDSQL_PROJECT-$LATEST.zip para commit"
 	echo "Ocurrió un error al añadir archivo $MDSQL_PROJECT-$LATEST.zip para commit"
 	exit
 fi
 
 git commit -m "Entregable $LATEST"
 if [ $? -ne 0 ] ; then
-	#notify-send --urgency=low "Ocurrió un error al hacer commit"
 	echo "Ocurrió un error al hacer commit"
 	exit
 fi
 
 git push
 if [ $? -ne 0 ] ; then
-	#notify-send --urgency=low "Ocurrió un error al hacer push"
 	echo "Ocurrió un error al hacer push"
 	exit
 fi
@@ -154,11 +152,9 @@ Cualquier error nos lo comunican lo más pronto posible para poder solucionarlo.
 Atentamente
 Federico Martín Lara <fmartin@esoluzion.com>" | mail -s "Entregable $LATEST" -aCC:$CC $TO
 if [ $? -ne 0 ] ; then
-	#notify-send --urgency=low "Ocurrió un error al enviar aviso"
 	echo "Ocurrió un error al enviar aviso"
 	exit
 fi
 
 $DIR_PROJECT/bin/entregar.sh $MDSQL_PROJECT $LATEST
-#notify-send --urgency=low "Entrega $LATEST completada"
 echo "Entrega $LATEST completada"
