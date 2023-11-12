a5b2712b9be2b9195af3087379de4b1c928a6329
Federico Martín Lara <federico@horizontefinal>
2023-11-12 23:33:36 +0100

    Se resuelve:
    - 0000128: Permisos Generales x Modelo x tipo de Obj: Error
    P_MNTO_PER_GENERAL

--- MDSQL/src/main/java/com/mdsql/utils/MDSQLAppHelper.java
+++ MDSQL/src/main/java/com/mdsql/utils/MDSQLAppHelper.java
@@ -14,6 +14,7 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -268,5 +269,9 @@ public class MDSQLAppHelper extends AppHelper {
 		} catch (Exception e) {
 			return Boolean.FALSE;
 		}
+	}
+	
+	public static Date toDate(Timestamp timestamp) {
+		return new Date(timestamp.getTime());
 	}
 }
