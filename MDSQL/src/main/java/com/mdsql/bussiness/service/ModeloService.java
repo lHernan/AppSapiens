7ce94dc99fdc8ea722646bc62e301d5a50096102
Federico Martín Lara <federico@horizontefinal>
2024-01-03 22:15:06 +0100

    Se resuelve:
    - 0000139: Pantalla mantenimiento variables – No se muestran los avisos

--- MDSQL/src/main/java/com/mdsql/bussiness/service/ModeloService.java
+++ MDSQL/src/main/java/com/mdsql/bussiness/service/ModeloService.java
@@ -1,10 +1,9 @@
 package com.mdsql.bussiness.service;
 
-import java.util.List;
-
 import com.mdsql.bussiness.entities.Modelo;
+import com.mdsql.bussiness.entities.OutputActualizarVariablesModelo;
 import com.mdsql.bussiness.entities.OutputConsultaModelos;
-import com.mdsql.bussiness.entities.Variable;
+import com.mdsql.bussiness.entities.OutputVariablesModelo;
 import com.mdval.exceptions.ServiceException;
 
 /**
@@ -22,9 +21,9 @@ public interface ModeloService {
 	OutputConsultaModelos consultaModelos(String codigoProyecto, String nombreModelo, String codigoSubProyecto)
 			throws ServiceException;
 
-	List<Variable> consultaVariables(Modelo modelo) throws ServiceException;
+	OutputVariablesModelo consultaVariables(Modelo modelo) throws ServiceException;
 
-	void actualizarVariableModelo(String codigoProyecto, String codigoVariable, String entorno, String bbdd,
+	OutputActualizarVariablesModelo actualizarVariableModelo(String codigoProyecto, String codigoVariable, String entorno, String bbdd,
 			String tipoVariable, String valorVariable, String valorSustituir, String codigoPeticion, String mcaInterno,
 			String mcaHabilitado, String comentario, String codUsr) throws ServiceException;
 }
--- MDSQL/src/main/java/com/mdsql/bussiness/service/impl/ModeloServiceImpl.java
+++ MDSQL/src/main/java/com/mdsql/bussiness/service/impl/ModeloServiceImpl.java
@@ -110,7 +110,7 @@ public class ModeloServiceImpl extends ServiceSupport implements ModeloService {
 	}
 
 	@Override
-	public List<Variable> consultaVariables(Modelo modelo) throws ServiceException {
+	public OutputVariablesModelo consultaVariables(Modelo modelo) throws ServiceException {
 		String runSP = createCall("p_con_vbles_modelo", MDSQLConstants.CALL_04_ARGS);
 
 		try (Connection conn = dataSource.getConnection();
@@ -133,6 +133,14 @@ public class ModeloServiceImpl extends ServiceSupport implements ModeloService {
 
 			if (result == 0) {
 				throw buildException(callableStatement.getArray(4));
+			}
+			
+			OutputVariablesModelo outputVariablesModelo = new OutputVariablesModelo();
+			outputVariablesModelo.setResult(result);
+			
+			// Hay avisos
+			if (result == 2) {
+				outputVariablesModelo.setServiceException(buildException(callableStatement.getArray(4)));
 			}
 
 			List<Variable> variables = new ArrayList<>();
@@ -163,8 +171,11 @@ public class ModeloServiceImpl extends ServiceSupport implements ModeloService {
 
 					variables.add(variable);
 				}
+				
+				outputVariablesModelo.setVariables(variables);
 			}
-			return variables;
+			
+			return outputVariablesModelo;
 		} catch (SQLException e) {
 			LogWrapper.error(log, "[ModeloService.consultaVariables] Error:  %s", e.getMessage());
 			throw new ServiceException(e);
@@ -172,7 +183,7 @@ public class ModeloServiceImpl extends ServiceSupport implements ModeloService {
 	}
 
 	@Override
-	public void actualizarVariableModelo(String codigoProyecto, String codigoVariable, String entorno, String bbdd, String tipoVariable, String valorVariable, String valorSustituir, String codigoPeticion, String mcaInterno, String mcaHabilitado, String comentario, String codUsr) throws ServiceException {
+	public OutputActualizarVariablesModelo actualizarVariableModelo(String codigoProyecto, String codigoVariable, String entorno, String bbdd, String tipoVariable, String valorVariable, String valorSustituir, String codigoPeticion, String mcaInterno, String mcaHabilitado, String comentario, String codUsr) throws ServiceException {
 		String runSP = createCall("p_mnto_vbles_modelo", MDSQLConstants.CALL_14_ARGS);
 
 		try (Connection conn = dataSource.getConnection();
@@ -205,7 +216,16 @@ public class ModeloServiceImpl extends ServiceSupport implements ModeloService {
 			if (result == 0) {
 				throw buildException(callableStatement.getArray(14));
 			}
+			
+			OutputActualizarVariablesModelo outputActualizarVariablesModelo = new OutputActualizarVariablesModelo();
+			outputActualizarVariablesModelo.setResult(result);
+			
+			// Hay avisos
+			if (result == 2) {
+				outputActualizarVariablesModelo.setServiceException(buildException(callableStatement.getArray(4)));
+			}
 
+			return outputActualizarVariablesModelo;
 		} catch (SQLException e) {
 			LogWrapper.error(log, "[ModeloService.actualizarVariableModelo] Error:  %s", e.getMessage());
 			throw new ServiceException(e);
